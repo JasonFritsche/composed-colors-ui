c504ba4780625ac6f7e9e87588f302ed0efb4f13
Jason Fritsche <fritsche.jason@gmail.com>
2021-01-28 20:23:11 -0600

    added sass mixin for breakpoints, updated custom theme, added scss variables file

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+const x = [MatSidenavModule, MatToolbarModule, MatIconModule];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatSidenavModule, MatToolbarModule],
-  exports: [MatSidenavModule, MatToolbarModule],
+  imports: [CommonModule, ...x],
+  exports: [...x],
 })
 export class MaterialModule {}

ba32266c2cc36769e64447b0e4090127f0e117ce
Jason Fritsche <fritsche.jason@gmail.com>
2021-02-13 17:10:05 -0600

    added logic to allow editing of color in palette creator

--- src/app/features/create-colors/create-colors/create-palette-dialog/create-palette-dialog.component.ts
+++ src/app/features/create-colors/create-colors/create-palette-dialog/create-palette-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms';
 import { ColorPickerControl, ColorsTable } from '@iplab/ngx-color-picker';
 import { Color } from 'src/app/shared/interfaces/Colors';
@@ -22,22 +22,12 @@ export class CreatePaletteDialogComponent implements OnInit {
     description: ['', Validators.required],
   });
 
+  selectedColor: Color | undefined;
+  selectedIndex!: number;
+  isColorSelected: boolean = false;
+
   get colors() {
     return this.colorPaletteForm.get('colors') as FormArray;
-  }
-
-  addColor() {
-    const newColor: Color = {
-      hsva: this.chromeControl.value.getHsva(),
-      rgba: this.chromeControl.value.getRgba(),
-    };
-
-    let newColorGroup: FormGroup = this._formBuilder.group({
-      color: [newColor],
-      colorName: [''],
-    });
-
-    this.colors.push(newColorGroup);
   }
 
   constructor(
@@ -46,6 +36,38 @@ export class CreatePaletteDialogComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
+
+  @HostListener('click', ['$event']) onClick(e: MouseEvent) {
+    const target = e.target as Element;
+    if (
+      target.parentNode?.nodeName.includes('CHROME-PICKER') ||
+      target.parentNode?.nodeName.includes('RGBA-INPUT-COMPONENT') ||
+      target.parentElement?.className.includes('column')
+    ) {
+      // keep selected color selected if the color picker is clicked
+      return;
+    } else if (!target?.className?.includes('color-circle')) {
+      this.isColorSelected = false;
+    }
+  }
+
+  addColor() {
+    const newColor: Color = {
+      hsva: this.chromeControl.value.getHsva(),
+      rgba: this.chromeControl.value.getRgba(),
+    };
+
+    if (this.isColorSelected) {
+      this.colors.at(this.selectedIndex).get('color')?.patchValue(newColor);
+    } else {
+      let newColorGroup: FormGroup = this._formBuilder.group({
+        color: [newColor],
+        colorName: [''],
+      });
+
+      this.colors.push(newColorGroup);
+    }
+  }
 
   getCircleColor(circle: Color): string {
     const { alpha, blue, green, red } = { ...circle.rgba };
@@ -59,5 +81,15 @@ export class CreatePaletteDialogComponent implements OnInit {
       description: this.colorPaletteForm.get('description')?.value,
     };
     this._colorsService.addUserColorPalette(colorPalette);
+  }
+
+  /**
+   * Called when clicking a color
+   * Sets the selectedColorObj so that the selected color can be edited/updated
+   * @param index
+   */
+  selectColor(index: number): void {
+    this.isColorSelected = true;
+    this.selectedIndex = index;
   }
 }

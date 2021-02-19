a84b1b93fbda841800c8ed5215702750755b28f0
Jason Fritsche <fritsche.jason@gmail.com>
2021-02-18 19:25:42 -0600

    initial work to display user created palettes

---
+++ src/app/shared/components/palette-display/colors-display/colors-display.component.ts
@@ -0,0 +1,41 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Color } from 'src/app/shared/interfaces/Colors';
+
+@Component({
+  selector: 'app-colors-display',
+  templateUrl: './colors-display.component.html',
+  styleUrls: ['./colors-display.component.scss'],
+})
+export class ColorsDisplayComponent implements OnInit, OnChanges {
+  @Input() colorObj: any;
+  circleColor: string;
+
+  constructor() {
+    this.circleColor = 'rgba(1,1,1,1)';
+  }
+
+  ngOnInit(): void {}
+
+  private _generateColor(): void {
+    const color: Color = JSON.parse(this.colorObj).color;
+    if (color.rgba) {
+      this._setCircleColor(color.rgba);
+    }
+  }
+
+  private _setCircleColor(rgba: Color['rgba']) {
+    this.circleColor = `rgba(${rgba.red},${rgba.green},${rgba.blue},${rgba.alpha})`;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.colorObj.currentValue) {
+      this._generateColor();
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaletteDisplayComponent } from './components/palette-display/palette-display.component';
+import { ColorsDisplayComponent } from './components/palette-display/colors-display/colors-display.component';
 
 @NgModule({
-  declarations: [PaletteDisplayComponent],
+  declarations: [PaletteDisplayComponent, ColorsDisplayComponent],
   imports: [CommonModule],
   exports: [PaletteDisplayComponent],
 })
